35dcbd7a5d912c9f6096f856b9dde741e1c2f905
Matt Rieger <mrieger@cars.com>
2015-12-07 10:15:43 -0600

    Chapter 7 complete.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Repo
+alias Rumbl.Category
+alias Rumbl.Video
+
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  if !Repo.get_by(Category, name: category) do
+   new_cat = Repo.insert!(%Category{name: category})
+   Repo.insert!(%Video{category_id: new_cat.id, url: "foo", title: "bar",
+    description: "baz"})
+  end
+end
