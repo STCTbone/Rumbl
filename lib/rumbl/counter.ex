d9cbc951b40df501bab340cafb613e58a035e05b
Matt Rieger <mrieger@cars.com>
2015-12-07 16:01:26 -0600

    WIP added GenServer counter lib.

---
+++ lib/rumbl/counter.ex
@@ -0,0 +1,31 @@
+defmodule Rumbl.Counter do
+  use GenServer
+
+  def inc(pid), do: GenServer.cast(pid, :inc)
+
+  def dec(pid), do: GenServer.cast(pid, :dec)
+
+  def val(pid, timeout \\ 5000) do
+    GenServer.call(pid, :val)
+  end
+
+  def start_link(initial_val) do
+    GenServer.start_link(__MODULE__, initial_val)
+  end
+
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
+end
